8ed7cff96aebb4c99a738e68fa494991d0949425
Nathaliavilas03 <nathaliavilasboas@hotmail.com>
2025-05-16 20:11:31 -0300

    Primeiro commit

---
+++ Atividades.py
@@ -0,0 +1,23 @@
+#Atividade 01
+print("Olá mundo!")
+
+#Atividade 02 - Desenvolva um programa que soma dois números. 
+#Use as variáveis numero1 = 12 e numero2 = 14. 
+#O programa deve calcular a soma e exibir o resultado
+
+var1 = 12
+var2 = 14
+
+soma = var1 + var2
+print(soma)
+
+#Atividade 03 - Calculadora de Volume - Crie um programa que calcula o volume de uma caixa retangular. 
+# Use as seguintes dimensões: * Comprimento: 12 cm * Largura: 14 cm * Altura: 20 cm 
+# O programa deve calcular o volume e exibir o resulta
+
+c=12
+l=14
+h=20
+
+cal = c * l * h
+print("O volume da caixa é de:", cal, "cm³")
